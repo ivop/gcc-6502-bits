5aeeead643a980446459ac04fece38987d848720
Ivo van Poorten <ivopvp@gmail.com>
2018-03-01 14:50:31 +0100

    new fputc implementation for mmach=atari

--- libtinyc/atari/fputc.c
+++ libtinyc/atari/fputc.c
@@ -1,10 +1,24 @@
 #include "stdio.h"
+#include "stdint.h"
+
+#define PUTCHAR 0x0b
+
+static uint16_t *const ICBALH = (uint16_t *) 0x0344;
+static uint16_t *const ICBLLH = (uint16_t *) 0x0348;
+static uint8_t *const ICCOM = (uint8_t *) 0x0342;
+static uint8_t buffer;
 
 int fputc (int c, FILE *f)
 {
-  register char x asm ("a") = c;
-  /* FIXME: actually call ciov to output character to screen  */
-  __asm__ __volatile__ ("jsr $ffee" : : "Aq" (x));
+    buffer = c;
+    if (buffer == '\n') buffer = 0x9b;
+    *ICBALH = (uint16_t)&buffer;
+    *ICBLLH = 1;
+    *ICCOM = PUTCHAR;
+
+  register char x asm ("x") = 0;
+  __asm__ __volatile__ (\
+          "jsr $e456" : : "Aq" (x) );
 
   return c;
 }
